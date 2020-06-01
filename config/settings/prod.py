ab5e10b3a2495f1efe6865e582d2e8e0f953d340
Kim-Ha-Jeong <46347830+Kim-Ha-Jeong@users.noreply.github.com>
2020-06-01 19:54:21 +0900

    encoding error solve

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,9 +5,8 @@ from .base import *
 #from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False  # 꼭 필요합니다.
-dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-with open(dir, 'secrets.json', 'rb', encoding = 'utf-8') as secret_file:
+with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'secrets.json', 'rb', encoding = 'utf-8') as secret_file:
     secrets = json.load(secret_file)
 
 ALLOWED_HOSTS = secrets['ALLOWED_HOST']
