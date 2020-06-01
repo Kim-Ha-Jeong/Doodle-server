16352b4e0607032062286b3ccdbc3e375a66512f
Kim-Ha-Jeong <46347830+Kim-Ha-Jeong@users.noreply.github.com>
2020-06-01 20:52:32 +0900

    error solve

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 
 DEBUG = False  # 꼭 필요합니다.
 dir = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'secrets.json')
-with open(dir, 'rb', encoding='utf-8') as secret_file:
+with open(dir, 'rb') as secret_file:
     secrets = json.load(secret_file)
 
 ALLOWED_HOSTS = secrets['ALLOWED_HOST']
