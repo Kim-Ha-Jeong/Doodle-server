9cdd693143a5b0828d33d94f6f2085650d9e0f02
Kim-Ha-Jeong <kha031821@gmail.com>
2020-06-27 21:16:01 +0900

    exclude password

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ('password', 'last_login')
-        fields = '__all__'
 
 
 class ReviewSerializer(serializers.ModelSerializer):
