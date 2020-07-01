1762e7e8afddc8e9bab352a6aa20d7eda1e20c03
Kim-Ha-Jeong <kha031821@gmail.com>
2020-07-01 19:19:24 +0900

    url change

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,5 @@ router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
 router.register(r'review', ReviewViewSet)
 urlpatterns = router.urls
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
