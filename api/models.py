f8c96aac26801c32f6cff465ff9925e566fb0205
Kim-Ha-Jeong <kha031821@gmail.com>
2020-07-03 21:02:44 +0900

    model modify - 2

--- api/models.py
+++ api/models.py
@@ -17,9 +17,10 @@ class Produce(models.Model):
     doodle = models.ImageField()
     redesign = models.IntegerField()
     receiver = models.CharField(max_length=20)
-    phone_num = models.CharField(max_length=20)
+    r_phone_num = models.CharField(max_length=20)
     post_code = models.CharField(max_length=100)
     base_address = models.CharField(max_length=100)
     detail_address = models.CharField(max_length=100)
-
-
+    order = models.CharField(max_length=20)
+    o_phone_num = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
