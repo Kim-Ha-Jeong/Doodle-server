71b0b196a62fff1b32572b21df43c4a23ff38758
Kim-Ha-Jeong <kha031821@gmail.com>
2020-07-03 21:14:48 +0900

    admin setting

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+
+class ProduceAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Produce, ProduceAdmin)
