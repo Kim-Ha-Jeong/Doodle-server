0ac406f5c751ec77d46e7bc73f3c915354551e86
Kim-Ha-Jeong <kha031821@gmail.com>
2020-07-08 18:02:41 +0900

    filter modify

--- api/filter.py
+++ api/filter.py
@@ -4,10 +4,11 @@ from api.models import *
 
 class ProduceFilter(FilterSet):
     order = filters.CharFilter(method='produce_order_filter')
-    #o_phone_num = filters.CharFilter(method='produce_phone_filter')
+    o_phone_num = filters.CharFilter(method='produce_order_filter')
 
-    def produce_order_filter(self, queryset, order, value):
-        order = self.request.query_params.get(order, value)
+    def produce_order_filter(self, queryset, order, o_phone_num, value1, value2):
+        order = self.request.query_params.get(order, value1)
         if order is not None:
-            queryset = queryset.filter(order__icontains=value)
+            if o_phone_num is not None:
+                queryset = queryset.filter(order__icontains=value1, o_phone_num__icontains=value2)
         return queryset
